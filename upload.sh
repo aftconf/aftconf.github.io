6b76553222ce500e5747913b399620935b291ab7
Bernhard Haslhofer <haslhofer@csh.ac.at>
2023-11-21 15:49:40 +0100

    fix mirror

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-lftp -e "put -O ./ editions.html; put -O ./ index.html; mirror -R aft23 aft23; exit"  -u webmaster@aft.acm.org aft.acm.org
+lftp -e "put -O ./ editions.html; put -O ./ index.html; mirror -R aft24 aft24; exit"  -u webmaster@aft.acm.org aft.acm.org
 
