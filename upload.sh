1c2ad781f70da12f4a444cf95d7bd41bda4caed9
Nicolas Christin <nicolasc@cmu.edu>
2024-11-26 13:05:37 -0500

    updated upload script

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-lftp -e "put -O ./ editions.html; put -O ./ index.html; mirror -R aft24 aft24; exit"  -u webmaster@aft.acm.org aft.acm.org
+lftp -e "put -O ./ editions.html; put -O ./ index.html; mirror -R aft25 aft25; exit"  -u webmaster@aft.acm.org aft.acm.org
 
